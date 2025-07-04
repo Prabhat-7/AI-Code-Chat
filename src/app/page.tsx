b531a7c1fddd3fa4bff3be7fcfda6942e6a23cfe
Prabhat-7 <prabhatsitaula@gmail.com>
2025-07-04 15:21:00 +0545

    Loading state for the ai messege was implimented and also overflow was handled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,32 +8,46 @@ import { Button } from "@/components/ui/button";
 import InputBox from "@/components/input-box";
 
 export default function Home() {
-  const [messeges, setMesseges] = useState<any>([]);
+  const [messeges, setMesseges] = useState<any[]>([]);
   const [solution, setSolution] = useState("");
+
   const [input, setInput] = useState("");
   const handleClick = async () => {
     if (input == "") return;
+
+    setMesseges((prev: any) => [
+      ...prev,
+      <HumanMessage HumanMessage={input} />,
+    ]);
+    const tempInput = input;
+    setInput("");
+
+    setMesseges((prev: any) => [
+      ...prev,
+      <AiMesage AiMessage={""} isLoading={true} />,
+    ]);
     const response = await fetch("api/solution", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ "data": input }),
+      body: JSON.stringify({ "data": tempInput }),
     });
     const data = await response.json();
-    setSolution(data.solution);
 
-    setMesseges((prev: any) => [
-      ...prev,
-      <HumanMessage HumanMessage={input} />,
-      <AiMesage AiMessage={data.solution} />,
-    ]);
+    setMesseges((prev: any) => {
+      const withoutLoading = prev.slice(0, -1);
+      return [
+        ...withoutLoading,
+        <AiMesage AiMessage={data.solution} isLoading={false} />,
+      ];
+    });
     setInput("");
   };
 
   return (
     <div>
-      <ol className="mt-3 pt-3">
+      <ol className="mt-3 pt-3 overflow-auto h-[73vh]">
         {messeges.map((item: any, index: number) => (
           <li key={index}>{item}</li>
         ))}
