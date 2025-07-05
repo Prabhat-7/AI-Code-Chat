86de237fa0c4b220d2d64f5a72ae8b3688b6f15f
Prabhat-7 <prabhatsitaula@gmail.com>
2025-07-05 08:24:41 +0545

    A little change in the logic , now everything is handled by the ai sdk, status:simple chat bot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,34 @@
 "use client";
-import AiMesage from "@/components/messeges/AiMesage";
+import AiMessage from "@/components/messeges/AiMesage";
 import React, { useState } from "react";
 import HumanMessage from "@/components/messeges/HumanMessage";
 import { useChat } from "@ai-sdk/react";
 import InputBox from "@/components/input-box";
 
 export default function Home() {
-  const { messeges } = useChat();
-  const [messeges, setMesseges] = useState<any[]>([]);
-  const [solution, setSolution] = useState("");
-  const [input, setInput] = useState("");
-  const handleClick = async () => {
-    if (input == "") return;
-
-    setMesseges((prev: any) => [
-      ...prev,
-      <HumanMessage HumanMessage={input} />,
-    ]);
-    const tempInput = input;
-    setInput("");
-
-    setMesseges((prev: any) => [
-      ...prev,
-      <AiMesage AiMessage={""} isLoading={true} />,
-    ]);
-    const response = await fetch("api/solution", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ "data": tempInput }),
-    });
-    const data = await response.json();
-
-    setMesseges((prev: any) => {
-      const withoutLoading = prev.slice(0, -1);
-      return [
-        ...withoutLoading,
-        <AiMesage AiMessage={data.solution} isLoading={false} />,
-      ];
-    });
-    setInput("");
-  };
+  const { messages, input, handleSubmit, setInput } = useChat();
 
   return (
     <div>
       <ol className="mt-3 pt-3 overflow-auto h-[73vh]">
-        {messeges.map((item: any, index: number) => (
-          <li key={index}>{item}</li>
+        {messages.map((message, index) => (
+          <div key={index}>
+            {message.parts.map((part, i) => {
+              switch (part.type) {
+                case "text": {
+                  switch (message.role) {
+                    case "user":
+                      return <HumanMessage HumanMessage={part.text} />;
+                    case "assistant":
+                      return <AiMessage AiMessage={part.text} />;
+                  }
+                }
+              }
+            })}
+          </div>
         ))}
       </ol>
-      <InputBox input={input} handleClick={handleClick} setInput={setInput} />
+      <InputBox input={input} handleClick={handleSubmit} setInput={setInput} />
     </div>
   );
 }
--- src/components/messeges/AiMesage.tsx
+++ src/components/messeges/AiMesage.tsx
@@ -3,19 +3,13 @@ import { Card } from "../ui/card";
 import { Brain } from "lucide-react";
 import Loading from "../loading-3dots";
 
-export default function AiMessage({
-  AiMessage,
-  isLoading,
-}: {
-  AiMessage: string;
-  isLoading: boolean;
-}) {
+export default function AiMessage({ AiMessage }: { AiMessage: string }) {
   return (
     <div className="flex gap-2 pl-2 my-2 mx-4">
       <div className=" w-max h-max bg-card p-1 rounded-full border border-muted-foreground/50">
         <Brain />
       </div>
-      {isLoading ? <Loading /> : <Card className="w-max p-3">{AiMessage}</Card>}
+      <Card className="w-max p-3">{AiMessage}</Card>
     </div>
   );
 }
